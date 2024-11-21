d89c4dc25e9b0da3941ddfe13df96d407f7b1cbc
Michael Correa <trabajosmichaelcorrea@gmail.com>
2024-11-21 10:46:56 -0500

    Departamentos completo

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,10 @@ route::get('/comunas/create',[ComunaController::class,'create'])->name('comunas.
 Route::delete('/comunas/{comuna}',[ComunaController::class,'destroy'])->name('comunas.destroy');
 Route::put('/comunas/{comuna}',[ComunaController::class,'update'])->name('comunas.update');
 Route::get('/comunas/{comuna}/edit',[ComunaController::class,'edit'])->name('comunas.edit');
-
+//RUTAS PARA DEPARTAMENTO
+Route::get('/departamentos', [DepartamentoController::class, 'index'])->name('departamentos.index');
+Route::post('/departamentos', [DepartamentoController::class, 'store'])->name('departamentos.store');
+Route::get('/departamentos/create', [DepartamentoController::class, 'create'])->name('departamentos.create');
+Route::delete('/departamentos/{departamento}', [DepartamentoController::class, 'destroy'])->name('departamentos.destroy');
+Route::put('/departamentos/{departamento}', [DepartamentoController::class, 'update'])->name('departamentos.update');
+Route::get('/departamentos/{departamento}/edit', [DepartamentoController::class, 'edit'])->name('departamentos.edit');
