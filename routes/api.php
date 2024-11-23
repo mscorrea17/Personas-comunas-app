68d0e5d6bdf525cd03863b56f991ca00aca5509a
Michael Correa <trabajosmichaelcorrea@gmail.com>
2024-11-23 10:29:38 -0500

    Actualizacion api.php

--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,22 @@
 <?php
 
-use App\Http\Controllers\api\ComunaController;
-use App\Http\Controllers\api\MunicipioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\Api\ComunaController;
+use App\Http\Controllers\Api\MunicipioController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
-Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas');
-Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
-Route::get('/comunas/{comuna}', [ComunaController::class, 'show'])->name('comunas.show');
-Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
+Route::controller(ComunaController::class)->group(function () {
+    Route::get('/comuna', 'index');
+    Route::post('/comuna', 'store');
+    Route::get('/comuna/{id}', 'show');
+    Route::put('/comuna/{id}', 'update');
+    Route::delete('/comuna/{id}', 'destroy');
+});
 
-Route::get('/municipios', [MunicipioController::class, 'index'])->name('municipios');
+Route::controller(MunicipioController::class)->group(function () {
+    Route::get('/municipios', 'index'); 
+});
